8ca47e37da8688b2ebf5439d6ab3c016ff170d89
ultralisk <lizhouml@amazon.com>
2020-06-09 16:36:58 -0700

    update file header

--- dstqa/dstqa_util.py
+++ dstqa/dstqa_util.py
@@ -1,4 +1,4 @@
-// Copyright <first-edit-year> Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: LicenseRef-.amazon.com.-AmznSL-1.0
 // Licensed under the Amazon Software License  http://aws.amazon.com/asl/
 
